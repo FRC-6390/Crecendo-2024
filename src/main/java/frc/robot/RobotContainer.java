99a91d1e1e90db0ff785179904a95fb65117201b
Mahdi <93354415+MahditS@users.noreply.github.com>
2024-09-28 16:46:15 -0700

    Robot Container Cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,36 +10,21 @@ import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain6390;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
-//import frc.robot.subsystems.Test;
 import frc.robot.utilities.controller.DebouncedController;
 import frc.robot.utilities.controller.DebouncedJoystick;
 import frc.robot.utilities.vission.LimeLight;
-import frc.robot.utilities.vission.LimelightHelpers;
 import frc.robot.commands.auto.TurnAlign;
-
-import java.util.List;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.commands.PathfindHolonomic;
-import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.pathfinding.Pathfinding;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -47,13 +32,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.*;
 import frc.robot.commands.auto.AutoFeed;
 import frc.robot.commands.auto.IntakeDrive;
-import frc.robot.commands.auto.Shoot;
 import frc.robot.commands.auto.TurnCommand;
 
 public class RobotContainer {
   
   public static Arm arm;
-//public static frc.robot.subsystems.Test test = new Test();
   public static LimeLight limelight = new LimeLight();
   public static double speed = -0.5;
   public static double threshold = 10;
@@ -63,18 +46,13 @@ public class RobotContainer {
   public static Intake intake = new Intake();
   public static Shooter shooter = new Shooter();
   public static Pose2d scoringPos = new Pose2d(1.24, 5.52, new Rotation2d());
-  public static Pose2d scoringPosR = new Pose2d(15.26, 5.52, new Rotation2d());
-// public static EventLoop eventLoop = new EventLoop();
-    
+  public static Pose2d scoringPosR = new Pose2d(15.26, 5.52, new Rotation2d()); 
   public static DebouncedController controller = new DebouncedController(0);
   private DebouncedJoystick joystick = new DebouncedJoystick(1);
-
- 
   public SendableChooser<String> autoChooser = new SendableChooser<>(); 
 
   public RobotContainer() {
     arm = new Arm(joystick);
-    //driveTrain.shuffleboard();
     driveTrain.init();
     autoChooser.addOption("Baseline", "Baseline");
     autoChooser.addOption("AutoNonStageBonus", "AutoNonStageBonus");
@@ -117,7 +95,6 @@ public class RobotContainer {
     NamedCommands.registerCommand("PivotMoveLow", new ArmTest(arm, 0));
     NamedCommands.registerCommand("PivotMoveHigh", new ArmTest(arm, -0.211));
     NamedCommands.registerCommand("Shoot", new ShooterRollers(-0.5, shooter, intake, 25));
-    // NamedCommands.registerCommand("WShoot", new ShooterRollers(1, shooter, intake, 80));
     NamedCommands.registerCommand("Feed", new Feed(-1, shooter, intake));
     NamedCommands.registerCommand("AutoFeed", new AutoFeed(-1, shooter, intake));
     NamedCommands.registerCommand("FastIntake", new IntakeDrive(driveTrain, 0, -0.8, 0, intake));
@@ -135,15 +112,6 @@ public class RobotContainer {
   {
 
     controller.start.whileTrue(new InstantCommand(driveTrain::zeroHeading));
-    
-    //Near Shot
-    //-9.21875
-    //-0.5
-
-    //Truss shot
-    // -17.355
-    //-0.65
-
 
   //---------------------------COMP CONTROLS---------------------------------//
     
@@ -204,112 +172,6 @@ public class RobotContainer {
 
   return new PathPlannerAuto(autoChooser.getSelected());
   // return new PathPlannerAuto("testauto");
-
-  //-----------------------Janus Autos---------------------------//
-
-    //Right Side 2 piece
-    // driveTrain.resetHeading();
-    // driveTrain.resetOdometry(new Pose2d(0,0, RobotContainer.driveTrain.getRotation2d()));
-    // return new SequentialCommandGroup
-    // (
-    // new JanusAuto(driveTrain, Constants.AUTO.RIGHT_SIDE_SEGMENT_1.build()),
-    // new TurnAlign(driveTrain, limelight, 0),
-    // new AutoAim(driveTrain, limelight, test)
-    // );
-
-    //Middle 2 piece
-    // driveTrain.resetHeading();
-    // driveTrain.resetOdometry(new Pose2d(0,0, RobotContainer.driveTrain.getRotation2d()));
-    // return new SequentialCommandGroup
-    // (
-    // new JanusAuto(driveTrain, Constants.AUTO.RIGHT_SIDE_SEGMENT_1.build()),
-    // new TurnAlign(driveTrain, limelight, 0),
-    // new AutoAim(driveTrain, limelight, test)
-    // );
-
-
-    //Left 2 piece
-    // driveTrain.resetHeading();
-    // driveTrain.resetOdometry(new Pose2d(0,0, RobotContainer.driveTrain.getRotation2d()));
-    // return new SequentialCommandGroup
-    // (
-    // new JanusAuto(driveTrain, Constants.AUTO.RIGHT_SIDE_SEGMENT_1.build()),
-    // new TurnAlign(driveTrain, limelight, 0),
-    // new AutoAim(driveTrain, limelight, test)
-    // );
-
-
-    //--------------------------Limelight Autos------------------------------//
-
-
-    //Mid auto skeleton
-    // return new SequentialCommandGroup
-    // (
-    //   new AutoDrive(driveTrain, limelight, 0, -0.2, 0),
-    //   new AutoAlign(driveTrain, limelight, -3.47, 0, 0, 0), new ArmTest(test, 0.5)
-    // );
-    
-    //Right Auto Skeleton
-    // return new SequentialCommandGroup
-    // (
-       
-    // new AutoDrive(driveTrain, limelight, 0, -0.75, 0),
-    // new AutoAlign(driveTrain, limelight, -2.65, 0, 0 , 0),
-    // new TurnAlign(driveTrain, limelight, 0)
-    
-    // );
-
-    //Right Side 2 PIECE skeleton
-    // return new SequentialCommandGroup
-    // (
-       
-    // new AutoDrive(driveTrain, limelight, 0, -0.75, 0),
-    // new AutoAlign(driveTrain, limelight, -2.75, 0, 0 , 0),
-    // new AutoAlign(driveTrain, limelight, 6.9, 20.77, 0, 0.03),
-    // new AutoAlign(driveTrain, limelight, -2.75, 0, 0 , 0),
-    // new TurnAlign(driveTrain, limelight, 0)
-    
-    // );
-
-    
-    //Janus Test Auto
-    // driveTrain.resetHeading();
-    // driveTrain.resetOdometry(new Pose2d(0,0, RobotContainer.driveTrain.getRotation2d()));
-    // return new SequentialCommandGroup
-    // (
-    // new JanusAuto(driveTrain, Constants.AUTO.RIGHT_SIDE_SEGMENT_1.build()),
-    // new TurnAlign(driveTrain, limelight, 0),
-    // new AutoAim(driveTrain, limelight, test)
-    // );
-
-    // controller.x.onTrue(Commands.runOnce(() -> 
-    // {
-     
-    //   Pose2d startPos = new Pose2d(driveTrain.getPose().getTranslation(), driveTrain.getRotation2d());
-    //   double ydis = scoringPos.getY() - startPos.getY();
-    //   double xdis = scoringPos.getX() - startPos.getX();
-    //   Pose2d endPos = new Pose2d(startPos.getTranslation().plus(new Translation2d(xdis, ydis)), new Rotation2d());
-    //   List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
-    //   PathPlannerPath path = new PathPlannerPath(
-    //     bezierPoints, 
-    //     new PathConstraints(
-    //       4.0, 4.0, 
-    //       Units.degreesToRadians(360), Units.degreesToRadians(540)
-    //     ),  
-    //     new GoalEndState(0.0, startPos.getRotation())
-    //   );
-
-    //   // Prevent this path from being flipped on the red alliance, since the given positions are already correct
-    //   path.preventFlipping = true;
-
-    //   AutoBuilder.followPath(path).schedule();
-
-
-    // }));
-
-
-
-
   }
 
 }
