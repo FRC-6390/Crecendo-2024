afb1be22c2ed6c2e36e488e4634a9febab6cbda9
Mahdi <93354415+MahditS@users.noreply.github.com>
2024-10-18 19:04:36 -0700

    refactor

--- src/main/java/frc/robot/commands/auto/AmpAlign.java
+++ src/main/java/frc/robot/commands/auto/AmpAlign.java
@@ -2,13 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.auto;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.auto.TurnCommand;
 import frc.robot.subsystems.Drivetrain6390;
 import frc.robot.utilities.vission.LimeLight;
 
--- src/main/java/frc/robot/commands/auto/AutoAim.java
+++ src/main/java/frc/robot/commands/auto/AutoAim.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.auto;
 
 
 // import edu.wpi.first.math.util.Units;
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Drivetrain6390;
 import frc.robot.RobotContainer;
-import frc.robot.commands.auto.TurnCommand;
+import frc.robot.commands.ArmTest;
 import frc.robot.subsystems.Arm;
 import frc.robot.utilities.vission.ShooterHelper;
 import frc.robot.utilities.vission.LimeLight;
